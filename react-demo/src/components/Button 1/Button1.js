16d26d96847fd1382d61991dc2a138a0de882fe4
Epo <116945790+EpoXD@users.noreply.github.com>
2023-02-26 20:44:41 +0100

    Update Button1.js

--- react-demo/src/components/Button 1/Button1.js
+++ react-demo/src/components/Button 1/Button1.js
@@ -10,14 +10,13 @@ const Button1 = () => {
             
             <div className="btn1-container">
                 <div className="btn1-container-inner"></div>
-            </div>
+             </div>
             <div className="btn1">
                 <button className="btn1-inner"></button>
-            </div>
+             </div>
             <div className="back-btn">
                 <button className="back-btn-inner" onClick={() => navigate("/main")}>Back to List</button>
             </div>
-
         </div>
         
         </>
